107487a3e2ed5d1f76947f22e11d4c64265879d5
joonhyuck <jh83370@naver.com>
2022-07-19 23:03:48 +0900

    Bean Validation - 수정에 적용.

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원 넘게 입력해주세요")  //객체 오류 검증-> 이 방법은 제약이 많음.
 public class Item {
 
     private Long id;
